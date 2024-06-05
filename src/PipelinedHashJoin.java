51f05f8046a3ccfb5d7838c35391a3b76f51b3d7
George M <giorgos_matlis@hotmail.com>
2024-06-05 15:25:25 +0300

    Update PipelinedHashJoin.java

--- src/PipelinedHashJoin.java
+++ src/PipelinedHashJoin.java
@@ -10,7 +10,7 @@ public class PipelinedHashJoin {
         String mmsaUrl = "jdbc:mysql://localhost:3306/CDC_BRFSS_MMSA";
         String llcpUrl = "jdbc:mysql://localhost:3306/CDC_BRFSS_LLCP";
         String dbUsername = "root";
-        String dbPassword = "gmaok25m8102000DIS@ email";
+        String dbPassword = "---";
 
 
         long startTime = System.currentTimeMillis();
